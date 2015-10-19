2e5931e5631eac2f38436be60c72ceb9ca013bb9
DonMehdi <mehdi.koraichi@gmail.com>
2015-10-19 18:06:11 +0300

    Premier commit fablab

---
+++ README.md
@@ -0,0 +1,4 @@
+Fablab
+======
+
+A Symfony project created on October 18, 2015, 9:03 pm.
---
+++ src/Fablab/FablabBundle/FablabFablabBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Fablab\FablabBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FablabFablabBundle extends Bundle
+{
+}
